0b83f6072e0b7d635382e2aaba2de28b3d248eb4
Daniel <dondaniel028@example.com>
2022-03-18 15:22:57 +0100

    Dan

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -21,11 +21,62 @@
 
         >
           <div class="absolute-bottom" v-show="edit" style="padding: 0; height: 30px" >
-            <q-btn no-caps icon="add"  unelevated label="Edit" style="width:100%;" size="0.7rem"/>
+            <q-btn no-caps icon="add" @click="editFormShow=true"  unelevated label="Edit" style="width:100%;" size="0.7rem"/>
           </div>
         </q-img>
         <q-space/>
       </div>
+
+      <q-dialog v-model="editFormShow" persistent>
+        <q-card>
+            <q-card-section class="items-center">
+                <div class="column q-mt-md">
+                    <div class="bg-white col q-px-md column justify-between q-pb-md" style="height:300px;border-radius:0 0 4px 4px">
+                        <div class = "q-mx-sm">
+                            <q-file
+                              v-model="selectedFile"
+                              label="Select New Profile Picture"
+                              square
+                              flat
+                              use-chips
+                              clearable
+                              accept=".csv,.txt,.xls,.xlsx,.doc,.docx,.pdf,.dbf,.zip,.rar,.7z,.jpg,.png,.gif"
+                              max-files="1"
+                              max-file-size="5120000"
+                            >
+                              <template v-slot:prepend>
+                                <q-icon name="attach_file" />
+                              </template>
+                            </q-file>
+                          
+                            <q-input v-model="name" label="Edit Name:" />
+                            <q-input v-model="rank" label="Edit Rank:" />
+                            <q-expansion-item
+                              expand-separator
+                              icon="lock"
+                              label="Edit password"
+                              class="q-mt-md rounded-borders"
+                            >
+                              <q-card>
+                                <q-card-section>
+                                  <div class = "q-my-sm">
+                                    <q-input v-model="password" label="New Password:" />
+                                    <q-input v-model="password" label="Enter Password Again:" />
+                                  </div>
+                                </q-card-section>
+                              </q-card>
+                            </q-expansion-item>
+                        </div>
+                    </div>
+                </div>
+            </q-card-section>
+
+            <q-card-actions align="right">
+            <q-btn flat label="Cancel" color="red"  v-close-popup />
+            <q-btn label="Save" color="negative" />
+            </q-card-actions>
+        </q-card>
+      </q-dialog>
 
       <div class="flex  items-center q-mx-auto q-my-md">
         <p class="q-my-auto q-mr-md text-weight-medium">User Name</p>
@@ -102,29 +153,25 @@
                         <q-item-label class="text-left text-subtitle1">Support</q-item-label>
                     </q-item-section>
                 </q-item>
+                <q-item clickable class=""
+                  active-class="my-menu-link"
+                  v-ripple
+                  
+                  @click="logout" style="width:70%" >
 
-                <q-item clickable class="q-mb-md q-px-xl"
-                active-class="bg-primary text-secondary"
-                v-ripple
-                :active="link === 'Admin'"
-                @click="link = 'Admin'"
-                style="border-radius: 25px"
-                to="/admin"
-                >
-
-                    <q-item-section avatar>
-                        <q-icon name="lock" size="1.5rem" />
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label class="text-left text-subtitle1">Admin Login</q-item-label>
-                    </q-item-section>
+                  <q-item-section avatar>
+                      <q-icon name="logout" size="2rem" />
+                  </q-item-section>
+                  <q-item-section>
+                      <q-item-label class="text-h6 text-center">Logout</q-item-label>
+                  </q-item-section>
                 </q-item>
             </q-list>
 
     </div>
 
 
-     <div class="q-py-none row  q-mt-xl" >
+     <!-- <div class="q-py-none row  q-mt-xl" >
               <q-space/>
               <q-item clickable class=""
                 active-class="my-menu-link"
@@ -140,7 +187,7 @@
                 </q-item-section>
                 </q-item>
                 <q-space/>
-            </div>
+            </div> -->
 
   </q-page>
 </template>
@@ -152,10 +199,25 @@ export default defineComponent({
   name: 'EssentialLink',
   data(){
     return{
-      edit: false
+      edit: false,
+      editFormShow: false,
+
+      selectedFile: null,
+      name: "",
+      rank: "",
+      password: ""
+
+    }
+  },
+  method: {
+    logout(){
+      localStorage.removeItem("adminToken");
+      localStorage.removeItem("userDept");
+      localStorage.removeItem("userToken");
+  
+      this.$router.replace('/');
     }
   }
-
 })
 </script>
 
