a126033a0c805b5feda1b6f8072d34f07e90e549
Daniel <dondaniel028@example.com>
2022-03-18 13:35:30 +0100

    Dan

--- env.js
+++ env.js
@@ -1,3 +1,6 @@
+const prod = "https://edefense.herokuapp.com/api";
+const local = "http://192.168.130.180:3000/api"
+
 export default {
-    backend: "https://edefense.herokuapp.com/api"
+    backend: local
 }
